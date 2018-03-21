44ffedb8af7ff3062f51a8ac4b9eb6bc703bf049
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-21 00:21:25 -0300

    Criando função responsável por recuperar o token associada a um usuário do telegram

--- evabot/database/connection.py
+++ evabot/database/connection.py
@@ -1,12 +1,19 @@
 import records
 from evabot.settings import DB
 
+
 class Connection(object):
     def __init__(self):
         self.db = records.Database(DB)
-    
+
     def get_token(self, telegram_id):
-        pass
+        token_query = self.db.query(
+            'select * from users where telegram_id = ' + telegram_id)
+        
+        if len(token_query) == 0:
+            return None
+        
+        return token_query.first()
 
     def create_token(self, response, telegram_id):
         pass
