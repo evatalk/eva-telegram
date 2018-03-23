3fab962dea1e04368324ca91fa57ca85cd99b80d
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-23 00:38:19 -0300

    Corrigindo problemas nos manipuladores da conexão com o banco de dados

--- evabot/database/connection.py
+++ evabot/database/connection.py
@@ -1,26 +1,25 @@
-import records
+import sqlite3
+
 from evabot.settings import DB
 
 
 class Connection(object):
     def __init__(self):
-        self.db = records.Database(DB)
-        self.conn = sqlite3.connect('clientes.db')
-        self.cursor = conn.cursor()
+        self.conn = sqlite3.connect(DB)
+        self.cursor = self.conn.cursor()
 
     def get_token(self, telegram_id):
-        token_query = self.db.query(
-            'select * from users where telegram_id = ' + telegram_id)
-        
-        if len(token_query) == 0:
-            return None
-        
-        return token_query.first()
+        self.cursor.execute(
+            "SELECT * FROM users where telegram_id = ?", (telegram_id,))
+        return self.cursor.fetchone()
 
     def create_token(self, name, access_token, telegram_id):
-        entry = self.cursor.execute("""
+        self.cursor.execute("""
             INSERT INTO users (nome, access_token, telegram_id)
             VALUES (?, ?, ?)
             """, (name, access_token, telegram_id))
-        
+
         self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
