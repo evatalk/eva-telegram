efc919c04b0c3564d1841fc139a7a513c8cf7b86
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-22 23:46:17 -0300

    Criando função para cadastrar usuário e token de acesso

--- evabot/database/connection.py
+++ evabot/database/connection.py
@@ -5,6 +5,8 @@ from evabot.settings import DB
 class Connection(object):
     def __init__(self):
         self.db = records.Database(DB)
+        self.conn = sqlite3.connect('clientes.db')
+        self.cursor = conn.cursor()
 
     def get_token(self, telegram_id):
         token_query = self.db.query(
@@ -15,5 +17,10 @@ class Connection(object):
         
         return token_query.first()
 
-    def create_token(self, response, telegram_id):
-        pass
+    def create_token(self, name, access_token, telegram_id):
+        entry = self.cursor.execute("""
+            INSERT INTO users (nome, access_token, telegram_id)
+            VALUES (?, ?, ?)
+            """, (name, access_token, telegram_id))
+        
+        self.conn.commit()
