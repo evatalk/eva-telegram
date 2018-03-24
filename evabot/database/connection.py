5a6cccc9415bd8e6e7637d50a09bfff1d4e3179d
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-23 23:27:11 -0300

    Criando método para atualizar etapa no processo de registro do usuário

--- evabot/database/connection.py
+++ evabot/database/connection.py
@@ -8,18 +8,38 @@ class Connection(object):
         self.conn = sqlite3.connect(DB)
         self.cursor = self.conn.cursor()
 
-    def get_token(self, telegram_id):
-        self.cursor.execute(
-            "SELECT * FROM users where telegram_id = ?", (telegram_id,))
-        return self.cursor.fetchone()
-
-    def create_token(self, name, access_token, telegram_id):
+    def register_token(self, name, access_token, telegram_id):
         self.cursor.execute("""
             INSERT INTO users (nome, access_token, telegram_id)
             VALUES (?, ?, ?)
             """, (name, access_token, telegram_id))
 
         self.conn.commit()
+
+    def get_token(self, telegram_id):
+        self.cursor.execute(
+            "SELECT * FROM users WHERE telegram_id = ?", (telegram_id,))
+
+        return self.cursor.fetchone()
+
+    def register_step(self, telegram_id):
+        self.cursor.execute("""
+            INSERT INTO registerstep (step, telegram_id)
+            VALUES (1, ?)
+            """, (telegram_id))
+
+        self.conn.commit()
+
+    def update_step(self, telegram_id):
+        self.cursor.execute("""
+            UPDATE registerstep SET step = 2 WHERE telegram_id = ?    
+            """, (telegram_id,))
+
+    def get_step(self, telegram_id):
+        self.cursor.execute(
+            "SELECT step FROM registerstep WHERE telegram_id = ?", (telegram_id,))
+
+        return self.cursor.fetchone()
 
     def close_connection(self):
         self.conn.close()
