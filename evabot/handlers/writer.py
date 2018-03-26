1a2683e6210e56c74c7beb1f56336322df68b9f5
Mauro de Carvalho <mdcg.dev@gmail.com>
2018-03-26 01:08:21 -0300

    Criando método para manipular os dados do histórico do aluno informado pela EVA - API

--- evabot/handlers/writer.py
+++ evabot/handlers/writer.py
@@ -7,3 +7,18 @@ class Jsonifier(object):
         }
 
         return json_response
+
+
+class HistoryResponseWriter(object):
+    @classmethod
+    def concatenate_data(cls, content):
+        BASE = "Curso: {}\nSituação da matrícula: {}\nSituação da turma: {}"
+        history = []
+        for data in content:
+            history.append(BASE.format(
+                data["course_name"],
+                data["enrollment_status"],
+                data["class_status"],
+            ))
+
+        return history
